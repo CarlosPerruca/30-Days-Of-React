2fd1de43729198502f268eb0292684aff3f4efad
Carlos P <carlos_dic1985@hotmail.com>
2022-03-21 17:21:39 +0100

    Exercise 1.5

---
+++ solutions/day-01/exercise.js
@@ -0,0 +1,60 @@
+const countries = [
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+];
+
+const webTechs = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Redux",
+  "Node",
+  "MongoDB",
+];
+
+//Declare an empty array;
+let emptyArr = [];
+
+//Declare an array with more than 5 number of elements
+console.log(countries);
+console.log(webTechs);
+
+//Find the length of your array
+console.log(`the length of Countries it is ${countries.length}`);
+console.log(webTechs.length);
+
+//Get the first item, the middle item and the last item of the array
+let firstCountry = countries[0];
+let middleCountry = Math.round(countries.length / 2);
+let lastCountry = countries.length - 1;
+
+console.log(
+  firstCountry,
+  ", ",
+  countries[middleCountry],
+  ", ",
+  countries[lastCountry]
+);
+
+let firstTech = webTechs[0];
+let middleTech = Math.round(webTechs.length / 2);
+let lastTech = webTechs.length - 1;
+
+console.log(firstTech);
+console.log(webTechs[middleTech]);
+console.log(webTechs[lastTech]);
+
+//Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+
+let mixedDataTypes = countries.concat(webTechs);
+console.log(mixedDataTypes);
