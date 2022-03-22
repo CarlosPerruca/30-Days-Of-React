4882beabf5377f1c2b75f55b3688a64123972180
Carlos P <carlos_dic1985@hotmail.com>
2022-03-22 17:49:52 +0100

    level 1 exercise complete

--- solutions/day-01/exercise.js
+++ solutions/day-01/exercise.js
@@ -1,3 +1,5 @@
+//*EXERCISE LEVEL 1
+
 const countries = [
   "Albania",
   "Bolivia",
@@ -59,7 +61,7 @@ let mixedDataTypes = countries.concat(webTechs);
 console.log(mixedDataTypes);
 
 //Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
-let itCompaines = [
+let itCompanies = [
   "Facebook",
   "Google",
   "Microsoft",
@@ -70,7 +72,46 @@ let itCompaines = [
 ];
 
 //Print the array using console.log()
-console.log(itCompaines);
+console.log(itCompanies);
 
 //Print the number of companies in the array
-console.log(itCompaines.length);
+console.log(itCompanies.length);
+
+//Print the first company, middle and last company
+let firstCompany = itCompanies[0];
+let middleCompany = Math.round(itCompanies.length / 2);
+let lastCompany = itCompanies.length - 1;
+
+console.log(firstCompany);
+console.log(itCompanies[middleCompany]);
+console.log(itCompanies[lastCompany]);
+
+//Print out each company
+itCompanies.forEach((company) => {
+  console.log(company);
+});
+
+//Change each company name to uppercase one by one and print them out
+itCompanies.forEach((company) => {
+  console.log(company.toUpperCase());
+});
+
+//Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+console.log(
+  `${itCompanies[0]}, ${itCompanies[1]}, ${itCompanies[2]}, ${itCompanies[3]}, ${itCompanies[4]}, ${itCompanies[5]} and ${itCompanies[6]} are big IT companies`
+);
+
+//Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
+
+function checkCompany(item) {
+  return itCompanies.forEach((company) => {
+    if (company === item) {
+      console.log(`We found the company ${company} in the array!`);
+    } else {
+      console.log("Company is not found");
+    }
+  });
+}
+checkCompany();
+
+//*EXERCISE LEVEL 2
