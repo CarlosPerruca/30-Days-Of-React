19f0eae3697d82b1088cd82f40a0e407ddcf4822
Carlos P <carlos_dic1985@hotmail.com>
2022-03-22 18:33:03 +0100

    trynig to link js arrays

---
+++ solutions/day-01/main.js
@@ -0,0 +1,8 @@
+import { default as countries } from "./countries";
+import { default as webTechs } from "./webTechs";
+
+// const [webTechs] = require("./webTechs").default;
+// const [countries] = require("./countries").default;
+
+console.log(countries);
+console.log(webTechs);
